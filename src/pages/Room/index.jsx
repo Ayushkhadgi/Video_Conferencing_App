55d5bd963c31045b7a3a6e0575f1dba9b533b826
Ayush Khadgi <107793290+Ayushkhadgi@users.noreply.github.com>
2023-09-27 03:15:32 +0530

    Api

---
+++ src/pages/Room/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useParams} from "react-router-dom";
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt'
+
+const RoomPage = () => {
+    const { roomId } = useParams();
+
+    const myMeeting = async (element) => {
+        const appId = 525653868;
+        const serverSecret = "36136a091d6cd0deabe22bd468a119e5";
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appId, serverSecret, roomId, Date.now().toString(), "Ayush Khadgi");
+        const zc = ZegoUIKitPrebuilt.create(kitToken);
+        zc.joinRoom({
+            container: element,
+            sharedLinks: [
+                {
+                    name: 'Copy Link',
+                    url: `http://localhost:3000/room/${roomId}`,
+                }
+            ],
+            scenario: {
+                mode: ZegoUIKitPrebuilt.GroupCall,
+            },
+        });
+    };
+    return (
+        <div>
+            <div ref={myMeeting}/>
+        </div>
+    );
+};
+export default RoomPage;
